b771b6934e2d849469f8cb95a082f1f6f4245260
MD. Abdul Hadi <69854379+Hadi0609@users.noreply.github.com>
2023-09-22 01:44:23 +0600

    Update get-user-stats.js

--- assets/js/get-user-stats.js
+++ assets/js/get-user-stats.js
@@ -1,22 +1,18 @@
-$(document).ready(function(){
-            load_stats();
-                function load_stats(){
-                    $.ajax({
-                        url:"/ajax.php?ajax=get-user-stats",
-                        type : 'GET',
-                        dataType : 'json',
-                        success:function(data){
-      
-                          document.getElementById("usershare").innerHTML = data.share;
- 
-                          document.getElementById("userdownload").innerHTML = data.download;
-         
-                          document.getElementById("sizeuser").innerHTML = data.size;
-   
-                          document.getElementById("userbroken").innerHTML = data.broken;
-
-                          document.getElementById("usertrash").innerHTML = data.trashed;
-                        }
-                    })
-                }
-       });
+$(document).ready(function () {
+    load_stats();
+    function load_stats() {
+        $.ajax({
+            url: "/ajax.php?ajax=get-user-stats",
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                document.getElementById("usershare").innerHTML = data.share;
+                document.getElementById("userdownload").innerHTML = data.download;
+                document.getElementById("sizeuser").innerHTML = data.size;
+                document.getElementById("userbroken").innerHTML = data.broken;
+                document.getElementById("usertrash").innerHTML = data.trashed;
+                document.getElementById("shared_space").innerHTML = data.storage;
+            }
+        })
+    }
+});
